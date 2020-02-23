9cf8ac09ce2af157dac603361bb1eda7ddeeb280
Collin Smith <collin.smith@iostudio.com>
2020-02-23 00:04:54 -0600

    production deploy

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/irongrove/" : "/",
   css: {
     loaderOptions: {
       scss: {
         data: `@import "@/assets/scss/variables.scss";`
       }
     }
-  },
-  publicPath: process.env.NODE_ENV === "production" ? "/irongrove/" : "/"
+  }
 };
